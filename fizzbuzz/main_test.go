ee0c3f66fce555dafe64de7f30c00f7c7a2e82cc
yutaChaos <y_kimura@r.recruit.co.jp>
2018-08-12 11:38:05 +0900

    Add FizzBuzz test pattern

--- fizzbuzz/main_test.go
+++ fizzbuzz/main_test.go
@@ -3,9 +3,27 @@ package main
 import "testing"
 
 func TestFizzBuzz(t *testing.T) {
-	actual := FizzBuzz(1)
-	expected := "1"
-	if expected != actual {
+	actual1 := FizzBuzz(1)
+	expected1 := "1"
+	if expected1 != actual1 {
+		t.Fatal("Test fail")
+	}
+
+	actual2 := FizzBuzz(3)
+	expected2 := "Fizz"
+	if expected2 != actual2 {
+		t.Fatal("Test fail")
+	}
+
+	actual3 := FizzBuzz(5)
+	expected3 := "Buzz"
+	if expected3 != actual3 {
+		t.Fatal("Test fail")
+	}
+
+	actual4 := FizzBuzz(15)
+	expected4 := "FizzBuzz"
+	if expected4 != actual4 {
 		t.Fatal("Test fail")
 	}
 }
