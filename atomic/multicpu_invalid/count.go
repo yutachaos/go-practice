1c279269dd5e14f9e2e5a308ff521d1dadaa7365
yutachaos <18604471+yutachaos@users.noreply.github.com>
2019-10-15 11:21:48 +0900

    Change package name

---
+++ atomic/multicpu_invalid/count.go
@@ -0,0 +1,20 @@
+package multicpu_invalid
+
+import (
+	"sync"
+)
+
+func count() int64 {
+
+	var count int64
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			count++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	return count
+}
